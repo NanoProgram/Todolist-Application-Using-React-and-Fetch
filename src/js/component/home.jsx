e74e549360928f528bb88163a57b276236b99735
NanoProgram <danielj.perezd@gmail.com>
2022-12-28 03:06:01 +0000

    ++

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,24 +3,32 @@ import { useEffect, useState } from "react";
 
 
 
-
+let count = 0
 
 //create your first component
 const Home = () => {
-	const[task, setTask] = useState([])
+	const[tasks, setTasks] = useState([])
 	const newTask = (e) => {
 		if(e.key === 'Enter'){
-		setTask (prev => [...prev, e.target.value])
+		setTasks (prev => [...prev, e.target.value])
+		count++
 		}
 	}
+	const tag = Object.keys(tasks);
+	console.log(tag)
+	const deleteTask = () => {
+		setTasks(tasks.filter((tasks) => tasks !== tasks));
+	}
+
   return (
     <div id="container" class="container container fluid">
       <h1>Todo List</h1>
       <input id="input" type="text" placeholder="Add Task ..." onKeyDown={e => newTask(e)} />
-	  {task.map((task) => {
-		return <h1>{task}</h1>
+	  {tasks.map((tasks, index) => {
+		return <h1 key={index} value={tasks}>{tasks}<button  type="button" class="btn-close" aria-label="Close" onClick={()=> deleteTask(tasks)}></button></h1>
 	  })
 	  }
+	  <h2>"Task: " {count<1? "No" : count}</h2>
     </div>
   );
 };
