d9140d607fc3e6b15f68d6dc4797f21b22453d95
NanoProgram <danielj.perezd@gmail.com>
2023-03-03 21:50:12 +0000

    90%

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -10,6 +10,48 @@ import { useEffect, useState } from "react";
 const Home = () => {
   const [tasks, setTasks] = useState([]);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "https://assets.breatheco.de/apis/fake/todos/user/nano",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const data = await response.json();
+        console.log(data);
+        setTasks(data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    async function updateTask() {
+      try {
+        const response = await fetch(
+          "https://assets.breatheco.de/apis/fake/todos/user/nano",
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(tasks),
+          }
+        )
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    updateTask();
+  }, [tasks]);
+
   const newTask = async (e) => {
     if (e.key === "Enter") {
       const newTask = {
@@ -35,70 +77,37 @@ const Home = () => {
       }
     }
   };
-  const deleteTask = async (task) => {
+  const deleteTask = (task) => {
     setTasks((prev) => prev.filter((t) => t !== task));
   };
 
-  useEffect(() => {
-    async function updateTask() {
-      try {
-        const response = await fetch(
-          "https://assets.breatheco.de/apis/fake/todos/user/nano",
-          {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(tasks),
-          }
-        )
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    updateTask();
-  }, [tasks]);
-  
-
-  const done = (index) => {
-    tasks[index].done = true;
-    console.log(tasks)
+  const deleteAllTask = () => {
+    setTasks((prev) => []);
   };
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch(
-          "https://assets.breatheco.de/apis/fake/todos/user/nano",
-          {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        const data = await response.json();
-        console.log(data);
-        setTasks(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchData();
-  }, []);
+  const done = (index) => {
+    const change = [...tasks];
+    change[index].done = true;
+    setTasks(change);
+    console.log(tasks)
+  };
 
   return (
     <div class="image-back">
       <div id="container" class="container-fluid post-it">
         <div class="try">
-          <h1 class="fontTodo">Todo List</h1>
+          <h1 class="fontTodo">Todo List<button type="button"
+            class="nuclear btn btn-outline-warning" onClick={() => deleteAllTask()}><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-radioactive" viewBox="0 0 16 16">
+            <path d="M8 1a7 7 0 1 0 0 14A7 7 0 0 0 8 1ZM0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Z"/>
+            <path d="M9.653 5.496A2.986 2.986 0 0 0 8 5c-.61 0-1.179.183-1.653.496L4.694 2.992A5.972 5.972 0 0 1 8 2c1.222 0 2.358.365 3.306.992L9.653 5.496Zm1.342 2.324a2.986 2.986 0 0 1-.884 2.312 3.01 3.01 0 0 1-.769.552l1.342 2.683c.57-.286 1.09-.66 1.538-1.103a5.986 5.986 0 0 0 1.767-4.624l-2.994.18Zm-5.679 5.548 1.342-2.684A3 3 0 0 1 5.005 7.82l-2.994-.18a6 6 0 0 0 3.306 5.728ZM10 8a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"/>
+          </svg></button></h1>
           <div id="center">
             <input
               id="input"
               type="text"
               placeholder="Add Task ..."
               onKeyDown={(e) => newTask(e)}
-            />
+            /> 
           </div>
           {tasks.map((task, index) => {
             return (
